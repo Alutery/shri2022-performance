c8b38950a1a77adf63057fc860150cc3d92b096e
Aliya Safina <aliya.safina.n@gmail.com>
2022-08-26 22:07:48 +0300

    Updates

--- index.bundle.js
+++ index.bundle.js
@@ -1,1 +1,178 @@
-(()=>{var e={657:(e,t,r)=>{"use strict";r.r(t)},415:(e,t,r)=>{"use strict";r.r(t)}},t={};function r(i){var n=t[i];if(void 0!==n)return n.exports;var a=t[i]={exports:{}};return e[i](a,a.exports,r),a.exports}r.r=e=>{"undefined"!=typeof Symbol&&Symbol.toStringTag&&Object.defineProperty(e,Symbol.toStringTag,{value:"Module"}),Object.defineProperty(e,"__esModule",{value:!0})},r(657),r(415),(()=>{function e(e,t,r){e.forEach((e=>{e.addEventListener(t,r)}))}document.addEventListener("DOMContentLoaded",(()=>{!function(t){let r=t.querySelector(".section__tab_active").dataset.id;const i=t.querySelectorAll(".section__tab"),n=Array.from(i).map((e=>e.dataset.id)),a=t.querySelector(".section__select");function s(e){const i=t.querySelector(`.section__tab[data-id=${e}]`),n=t.querySelector(`.section__panel[data-id=${e}]`),s=t.querySelector(".section__tab_active"),c=t.querySelector(".section__panel:not(.section__panel_hidden)");r=e,s.classList.remove("section__tab_active"),s.setAttribute("aria-selected","false"),s.removeAttribute("tabindex"),i.classList.add("section__tab_active"),i.setAttribute("aria-selected","true"),i.setAttribute("tabindex","0"),i.focus({preventScroll:!0}),c.classList.add("section__panel_hidden"),c.setAttribute("aria-hidden","true"),n.classList.remove("section__panel_hidden"),n.setAttribute("aria-hidden","false"),a.value=e}a.addEventListener("input",(()=>{s(a.value)})),e(i,"click",(e=>{s(e.target.dataset.id)})),e(i,"keydown",(e=>{if(e.ctrlKey||e.metaKey||e.shiftKey||e.altKey)return;e.preventDefault();let t=n.indexOf(r);if(37===e.which)--t;else if(39===e.which)++t;else if(36===e.which)t=0;else{if(35!==e.which)return;t=n.length-1}t>=n.length?t=0:t<0&&(t=n.length-1),s(n[t])}))}(document.querySelector(".main__devices")),function(e){let t=!1;const r=document.querySelector(".header__links");e.addEventListener("click",(()=>{t=!t,e.setAttribute("aria-expanded",t?"true":"false"),e.querySelector(".header__menu-text").textContent=t?"Закрыть меню":"Открыть меню",r.classList.toggle("header__links_opened",t),r.classList.add("header__links-toggled")}))}(document.querySelector(".header__menu"))}))})()})();
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 657:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ }),
+
+/***/ 415:
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+// extracted by mini-css-extract-plugin
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be isolated against other modules in the chunk.
+(() => {
+__webpack_require__(657);
+__webpack_require__(415);
+
+(() => {
+    function bind(nodes, event, handler) {
+        nodes.forEach(node => {
+            node.addEventListener(event, handler);
+        });
+    }
+
+    function makeTabs() {
+        const node = document.querySelector('.main__devices');
+
+        let selected = node.querySelector('.section__tab_active').dataset.id;
+        const tabs = node.querySelectorAll('.section__tab');
+        const list = Array(tabs.length).fill()
+        tabs.forEach((tab, index) => {
+            list[index] = tab.dataset.id
+        })
+        const select = node.querySelector('.section__select');
+
+        function selectTab(newId) {
+            const newTab = node.querySelector(`.section__tab[data-id=${newId}]`);
+            const newPanel = node.querySelector(`.section__panel[data-id=${newId}]`);
+            const oldTab = node.querySelector('.section__tab_active');
+            const oldPanel = node.querySelector('.section__panel:not(.section__panel_hidden)');
+
+            selected = newId;
+
+            oldTab.classList.remove('section__tab_active');
+            oldTab.setAttribute('aria-selected', 'false');
+            oldTab.removeAttribute('tabindex');
+            newTab.classList.add('section__tab_active');
+            newTab.setAttribute('aria-selected', 'true');
+            newTab.setAttribute('tabindex', '0');
+            newTab.focus({
+                preventScroll: true
+            });
+
+            oldPanel.classList.add('section__panel_hidden');
+            oldPanel.setAttribute('aria-hidden', 'true');
+            newPanel.classList.remove('section__panel_hidden');
+            newPanel.setAttribute('aria-hidden', 'false');
+
+            select.value = newId;
+        }
+
+        select.addEventListener('input', () => {
+            selectTab(select.value);
+        });
+
+        bind(tabs, 'click', event => {
+            const newId = event.target.dataset.id;
+            selectTab(newId);
+        });
+
+        bind(tabs, 'keydown', event => {
+            if (event.ctrlKey || event.metaKey || event.shiftKey || event.altKey) {
+                return;
+            }
+            event.preventDefault();
+
+            let index = list.indexOf(selected);
+            if (event.which === 37) {
+                // left
+                --index;
+            } else if (event.which === 39) {
+                // right
+                ++index;
+            } else if (event.which === 36) {
+                // home
+                index = 0;
+            } else if (event.which === 35) {
+                // end
+                index = list.length - 1;
+            } else {
+                return;
+            }
+
+            if (index >= list.length) {
+                index = 0;
+            } else if (index < 0) {
+                index = list.length - 1;
+            }
+
+            selectTab(list[index]);
+        });
+    }
+
+    function makeMenu() {
+        const menu = document.getElementById('header-menu');
+        let expanded = false;
+        const links = document.getElementById('header-links');
+
+        menu.addEventListener('click', () => {
+            expanded = !expanded;
+            menu.setAttribute('aria-expanded', expanded ? 'true' : 'false');
+            menu.querySelector('.header__menu-text').textContent = expanded ? 'Закрыть меню' : 'Открыть меню';
+            links.classList.toggle('header__links_opened', expanded);
+            links.classList.add('header__links-toggled');
+        });
+    }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        makeTabs();
+        makeMenu();
+    });
+})();
+
+})();
+
+/******/ })()
+;
